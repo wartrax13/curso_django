62f4177fef0118e88ad72f4ae439df73bb6f7191
Pedro <pedro.moisesandrade@gmail.com>
2021-07-04 23:37:03 -0300

    acertando flake8
    close #5

--- pypro/settings.py
+++ pypro/settings.py
@@ -12,12 +12,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdir'. # noqa
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+# Quick-start development settings - unsuitable for production # noqa
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/ # noqa
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY: str = 'django-insecure-*%*g=@2h&0vi$b54_clakx5p&=)v+ta2n57rep)p_-l+=-#605'
