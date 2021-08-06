17a1cf71967306580c317cad2cdfb0b797c9ae40
Pedro <pedro.moisesandrade@gmail.com>
2021-08-06 16:13:51 -0300

    Acesso ao método no Template

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,11 +1,24 @@
+from django.urls import reverse
+
 from django.shortcuts import render
 
+
+class Video:
+    def __init__(self, slug, titulo, vimeo_id):
+        self.titulo = titulo
+        self.slug = slug
+        self.vimeo_id = vimeo_id
+
+    def get_absolute_url(self):
+        return reverse('aperitivos:video', args=(self.slug,))
+
+
 videos = [
-    {'slug': 'motivacao', 'titulo': 'Video Aperitivos: Miles Davis', 'vimeo_id': 578570237},
-    {'slug': 'instalacao-windows', 'titulo': 'Instalação Windows', 'vimeo_id': 578570237},
+    Video('motivacao', 'Video Aperitivos: Miles Davis', 578570237),
+    Video('instalacao-windows', 'Instalação Windows', 578570237),
 ]
 
-videos_dct = {dct['slug']: dct for dct in videos}
+videos_dct = {v.slug: v for v in videos}
 
 
 def indice(request):
